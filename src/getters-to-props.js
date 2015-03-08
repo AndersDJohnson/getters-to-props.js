dffca0af6f7e8c93dbee27e821c9ee84e638726d
Anders D. Johnson <anders.d.johnson.developer@gmail.com>
2015-03-07 19:41:16 -0600

    reindent

--- src/getters-to-props.js
+++ src/getters-to-props.js
@@ -11,52 +11,52 @@
   }
 }(this, function () {
 
-    var isFunction = function (obj) {
-      // console.log('obj:', obj);
-      var str = Object.prototype.toString.call(obj);
-      // console.log('str:', str);
-      return str === '[object Function]';
-    };
+  var isFunction = function (obj) {
+    // console.log('obj:', obj);
+    var str = Object.prototype.toString.call(obj);
+    // console.log('str:', str);
+    return str === '[object Function]';
+  };
 
-    var firstToLower = function (str) {
-      if (str.length === 0) return str;
-      return str[0].toLowerCase() + str.slice(1);
-    };
+  var firstToLower = function (str) {
+    if (str.length === 0) return str;
+    return str[0].toLowerCase() + str.slice(1);
+  };
 
-    var parseFunctionName = function (name) {
-      return name.match(/get(.+)/);
-    };
+  var parseFunctionName = function (name) {
+    return name.match(/get(.+)/);
+  };
 
-    var gettersToProps = function (obj) {
-      // console.log('obj:', obj);
-      if (! obj) {
-        return;
-      }
-      var props = {};
-      var key;
-      for (key in obj) {
-        var val = obj[key];
-        if (isFunction(val)) {
-          var parsed = parseFunctionName(key);
-          // console.log(parsed);
-          if (parsed) {
-            var prop = parsed[1];
-            prop = firstToLower(prop);
-            try {
-              props[prop] = val();
-            }
-            catch (e) {
-              // undefined indicates error calling getter with no arguments
-              props[prop] = undefined;
-            }
+  var gettersToProps = function (obj) {
+    // console.log('obj:', obj);
+    if (! obj) {
+      return;
+    }
+    var props = {};
+    var key;
+    for (key in obj) {
+      var val = obj[key];
+      if (isFunction(val)) {
+        var parsed = parseFunctionName(key);
+        // console.log(parsed);
+        if (parsed) {
+          var prop = parsed[1];
+          prop = firstToLower(prop);
+          try {
+            props[prop] = val();
+          }
+          catch (e) {
+            // undefined indicates error calling getter with no arguments
+            props[prop] = undefined;
           }
         }
-        else {
-          // console.log('Not a function: ' + key);
-        }
       }
-      return props;
-    };
+      else {
+        // console.log('Not a function: ' + key);
+      }
+    }
+    return props;
+  };
 
-    return gettersToProps;
+  return gettersToProps;
 }));
